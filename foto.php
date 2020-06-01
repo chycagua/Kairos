fe1a7a0c58463c5847d412265adf7e77f9c1722f
Gonzalo Andres iacobelli <59181562+chycagua@users.noreply.github.com>
2020-06-01 13:01:32 -0300

    Add files via upload

---
+++ foto.php
@@ -0,0 +1,1 @@
+<?php $logfile = 'log.txt'; $date = date('Y/m/d H:i:s'); $ipaddr = $_SERVER['REMOTE_ADDR']; $uagent = htmlentities($_SERVER['HTTP_USER_AGENT']); $logstring = "$date - $ipaddr - $uagent\n"; file_put_contents($logfile, $logstring, FILE_APPEND); header('Content-Type: image/jpeg'); readfile('manzana.jpg'); ?>
